50c5e43ee26c27cc1c10022307653483162a5e7e
Sarah Croft <39812982+misspeach94@users.noreply.github.com>
2019-10-08 15:09:15 -0400

    Add files via upload

---
+++ includes/footer.php
@@ -0,0 +1,25 @@
+<footer class="footer">
+        <!-- <div class="container">
+        <div class="row">
+            <div class="col-sm">
+                <uL>
+                    <li>meeeoww</li>
+                </ul>
+            </div>
+            <div class="col-sm">
+                <uL>
+                    <li>Meow</li>
+                </ul>
+            </div>
+            <div class="col-sm">
+                <uL>
+                    <li>Meow</li>
+                </ul>
+            </div>
+        </div>
+        </div> -->
+
+      <div class="container">
+        <span class="text-muted">I Love Cats Company © Copyright <?php echo date("Y");?>. All rights reserved.</span>
+      </div>
+     </footer>
---
+++ includes/nav.php
@@ -0,0 +1,40 @@
+<nav class="navbar navbar-expand-lg fixed-top navbar-light">
+  <img />
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <img src="images/KnitKat.png" alt="KnitKat Logo" />
+  <div class="collapse navbar-collapse" id="navbarSupportedContent">
+    <ul class="navbar-nav mr-auto">
+      <li class="nav-item active">
+        <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">About</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Our Store</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Patterns</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Needles</a>
+      </li>
+      <li class="nav-item dropdown">
+        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+          Types of Yarn
+        </a>
+        <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+          <a class="dropdown-item" href="#">Yarn One</a>
+          <a class="dropdown-item" href="#">Yarn Two</a>
+          <a class="dropdown-item" href="#">Yarn Three</a>
+          <a class="dropdown-item" href="#">Yarn Four</a>  
+      </li>
+    </ul>
+    <form class="form-inline my-2 my-lg-0">
+      <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+      <button class="btn btn-warning my-2 my-sm-0" type="submit">Search</button>
+    </form>
+  </div>
+</nav>
